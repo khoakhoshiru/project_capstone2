f0260fbc417ba987586d029b34fca3fac461cfbc
nguyenleanhkhoa <nguyenleanhkhoa857@gmail.com>
2022-10-21 23:00:59 +0700

    new

---
+++ module/project.js
@@ -0,0 +1,12 @@
+function Product(id, name, price, img, screen, backCamera, frontCamera, decs) {
+  {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.screen = hinhAnh;
+    this.backCamera = backCamera;
+    this.frontCamera = frontCamera;
+    this.img = img;
+    this.decs = decs;
+  }
+}
